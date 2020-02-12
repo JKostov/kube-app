6be9ec0831bbb8347c8c73e4254b79b50a87a5fd
JKostov <julijekos@gmail.com>
2020-02-13 00:13:08 +0100

    Added kafka connection

--- kube-cluster/src/main.ts
+++ kube-cluster/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('/cluster');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.enableCors();
+  const micro = app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'kube-cluster',
+        brokers: [configService.get('KAFKA_URL')],
+      }
+    },
+  });
+  micro.useGlobalPipes(new ValidationPipe());
+
+  micro.listen(() => console.log('Connected to kafka.'));
+  await app.listen(configService.get('APP_PORT'));
 }
 bootstrap();
